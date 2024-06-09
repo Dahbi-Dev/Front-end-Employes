654efb1ddd45a2fee71df3494f56d9ac3f0a4a55
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-06-09 23:25:37 +0100

    final

--- src/components/Q2.jsx
+++ src/components/Q2.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React, { useState } from "react";
+import NavBar from "./NavBar";
 
-function Q2() {
+function Q2({ data }) {
   return (
     <div>
       <NavBar />
+
+      <div
+        className="Table"
+        style={{ border: "black 1px solid", margin: "20px" }}
+      >
+        <h2>Liste des Employes</h2>
+        <table
+          style={{
+            borderCollapse: "collapse",
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <thead>
+            <tr style={{ backgroundColor: "#f2f2f2" }}>
+              <th style={{ padding: "10px", border: "1px solid #ddd" }}>nom</th>
+              <th style={{ padding: "10px", border: "1px solid #ddd" }}>
+                prenom
+              </th>
+              <th style={{ padding: "10px", border: "1px solid #ddd" }}>
+                posts
+              </th>
+              <th style={{ padding: "10px", border: "1px solid #ddd" }}>
+                departments
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id} style={{ border: "1px solid #ddd" }}>
+                <td style={{ padding: "10px", border: "1px solid #ddd" }}>
+                  {item.nomEmp}
+                </td>
+                <td style={{ padding: "10px", border: "1px solid #ddd" }}>
+                  {item.prenomEmp}
+                </td>
+                <td style={{ padding: "10px", border: "1px solid #ddd" }}>
+                  {item.poste}
+                </td>
+                <td style={{ padding: "10px", border: "1px solid #ddd" }}>
+                  {item.departement.nomDep}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Q2
+export default Q2;
--- src/components/Q3.jsx
+++ src/components/Q3.jsx
@@ -1,12 +1,82 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React, { useEffect, useState } from "react";
+import NavBar from "./NavBar";
 
-function Q3() {
+function Q3({ data }) {
+  const [input, setInput] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    if (input.trim() !== "") {
+      // Filter suggestions based on input
+      const filteredSuggestions = data.filter((item) =>
+        item.departement.nomDep.toLowerCase().startsWith(input.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  }, [input, data]);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSelectSuggestion = (suggestion) => {
+    setInput(suggestion.departement.nomDep);
+    setSuggestions([]); // Clear suggestions
+    handleSearch(suggestion.departement.nomDep);
+  };
+
+  const handleSearch = (query) => {
+    const found = data.filter(
+      (item) => item.departement.nomDep.toLowerCase() === query.toLowerCase()
+    );
+
+    if (found.length > 0) {
+      const names = found.map((item) => `${item.nomEmp} ${item.prenomEmp}`);
+      setResult(names.join(", "));
+    } else {
+      setResult("Departement Not Found");
+    }
+  };
+
   return (
     <div>
       <NavBar />
+
+      <div>
+        <h1>Recherche par Departement</h1>
+        <div style={{ display: "grid", margin: "50px", gap: "9px" }}>
+          <label>Entrer le nom du departement : </label>
+          <input
+            value={input}
+            type="text"
+            onChange={handleChange}
+            required
+          />
+         
+            {suggestions.map((suggestion) => (
+              <p style={{textDecoration:'underline', cursor:'pointer', color:'blue'}}
+                              key={suggestion.id}
+                onClick={() => handleSelectSuggestion(suggestion)}
+              >
+                {suggestion.departement.nomDep}
+              </p>
+            ))}
+        
+        </div>
+      </div>
+      <div>
+        <h1>Result</h1>
+        <ul>
+          {result.split(", ").map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Q3
+export default Q3;
